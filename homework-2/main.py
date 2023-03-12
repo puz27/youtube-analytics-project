e500a2709d54cbfc8ced9f8d4d4c833576a4ef8f
Nick_Linux <n.avramenko87@gmail.com>
2023-03-12 12:32:41 +0300

    final version

--- homework-2/main.py
+++ homework-2/main.py
@@ -1,9 +1,7 @@
 from src.channel import Channel
-from src.utils import print_info
 
 if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-
 
     # получаем значения атрибутов
     print(vdud.title)  # вДудь
@@ -14,11 +12,9 @@ if __name__ == '__main__':
     # vdud.channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
-
     # можем получить объект для работы с API вне класса
     print(Channel.get_service())
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
-
 
     # создаем файл 'vdud.json' в данными по каналу
     vdud.to_json('vdud.json')
