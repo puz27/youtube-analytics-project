4cd357df727541ca4aaeb54edbec5cff9deea193
anv <anv@woori.ru>
2023-03-17 16:47:25 +0300

    add 2 classes in srv video

---
+++ src/video.py
@@ -0,0 +1,19 @@
+from src.utils import get_video_statistic, get_video_statistic_by_playlist_id
+
+
+class Video:
+    def __init__(self, video_id: str) -> None:
+        self.video_title, self.view_count, self.video_url, self.like_count,\
+        self.comment_count = get_video_statistic(video_id)
+
+    def __str__(self):
+        return f"{self.video_title}"
+
+
+class PLVideo:
+    def __init__(self, video_id,  playlist_id):
+        self.video_title, self.view_count, self.video_url, self.like_count, \
+        self.comment_count = get_video_statistic_by_playlist_id(video_id, playlist_id)
+
+    def __str__(self):
+        return f"{self.video_title}"
