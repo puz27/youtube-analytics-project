f469430386afa04346927121b20f36c2335693a4
anv <anv@woori.ru>
2023-03-22 17:59:53 +0300

    add class playlist

--- src/playlist.py
+++ src/playlist.py
@@ -1,2 +1,8 @@
+from src.utils import get_playlist_info
+
+
 class PlayList:
-    pass
+
+    def __init__(self, playlist_id):
+        self.playlist_id = playlist_id
+        self.url, self.title = get_playlist_info(playlist_id)
